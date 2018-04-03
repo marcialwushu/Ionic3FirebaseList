3bc903b5c815a3c98c4f9297b7d944d57110e49b
marcialwushu <marcialwushu@gmail.com>
2018-04-03 14:48:19 -0300

    Adding AngularFire2 and Firebase config credentials

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { FIREBASE_CONFIG } from './firebase.credentials';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { MyApp } from './app.component';
 
 @NgModule({
@@ -13,7 +17,9 @@ import { MyApp } from './app.component';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDXTL8wWsrtRyiemHXPY7rNbBVGTzgPPxA",
+    authDomain: "contactlist-228bd.firebaseapp.com",
+    databaseURL: "https://contactlist-228bd.firebaseio.com",
+    projectId: "contactlist-228bd",
+    storageBucket: "contactlist-228bd.appspot.com",
+    messagingSenderId: "784409567848"
+}
